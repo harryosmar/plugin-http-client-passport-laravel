8ec8f9f3ea4cac6c76019f34d72bb5eae1449c5d
Harry Sitohang <harry@olx.co.id>
2017-03-31 09:38:27 +0700

    implement di container

---
+++ services/ServiceContainer.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: harry
+ * Date: 3/31/17
+ * Time: 9:25 AM
+ */
+
+namespace PluginHttpClientPassportLaravel\services;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Routing\Loader\YamlFileLoader;
+
+class ServiceContainer
+{
+    /**
+     * @var self
+     */
+    private static $instance;
+
+    /**
+     * @var ContainerBuilder
+     */
+    private $container;
+
+    public static function getInstance()
+    {
+        if (is_null(self::$instance)) {
+            self::$instance = new self();
+        }
+
+        return self::$instance;
+    }
+
+    private function __construct()
+    {
+        $this->container = new ContainerBuilder();
+
+        $loader = new YamlFileLoader($this->container, new FileLocator(__DIR__));
+        $loader->load('services.yml');
+    }
+
+    public function getContainer()
+    {
+        return $this->container;
+    }
+}
